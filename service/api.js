0b10d21240bb8d79bf1e9192e53830b40837548d
ziyeziye <547562496@qq.com>
2020-03-29 18:30:57 +0800

    api.js

--- service/api.js
+++ service/api.js
@@ -2,6 +2,7 @@ import MinRequest from './MinRequest';
 import store from '@/store/index';
 
 const minRequest = new MinRequest()
+const newRequest = new MinRequest()
 
 // 请求拦截器
 minRequest.interceptors.request((request) => {
@@ -15,18 +16,18 @@ minRequest.interceptors.request((request) => {
 
 // 响应拦截器
 minRequest.interceptors.response((response) => {
-	if(response.data.code == 401 || response.data.code == 402) {
-		store.commit("USER_INFO", null);
-	}
+    if (response.data.code == 401 || response.data.code == 402) {
+        store.commit("USER_INFO", null);
+    }
     return response.data
 })
 
 // 设置默认配置
 minRequest.setConfig((config) => {
     let api_token = uni.getStorageSync('api_token', '')
-	// 192.168.123.224
-	// 192.168.1.3 192.168.123.90
-    config.baseURL = 'http://192.168.50.29:8182'
+    // 192.168.1.3 192.168.123.90 192.168.123.224
+    // 192.168.50.29
+    config.baseURL = 'http://192.168.123.224:8182'
     config.header = {
         'content-type': 'application/json',
         'X-Requested-With': 'XMLHttpRequest',
@@ -76,13 +77,13 @@ export default {
         user() {
             return minRequest.get('/api/user')
         },
-		/**
-		 * 修改用户信息
-		 * @returns {Promise<unknown>}
-		 */
-		user_modify() {
-		    return minRequest.put('/api/users')
-		},
+        /**
+         * 修改用户信息
+         * @returns {Promise<unknown>}
+         */
+        user_modify() {
+            return minRequest.put('/api/users')
+        },
         /**
          * 获取文章列表
          * @param articleId
@@ -187,59 +188,105 @@ export default {
         follows_tag(id) {
             return minRequest.put('/api/tags/' + id + '/follow')
         },
-		/**
-		 * 获取浏览历史
-		 * @param data
-		 * @returns {Promise | Promise<unknown>}
-		 */
-		get_history(data) {
-		    return minRequest.get('/api/histories', data)
-		},
-		/**
-		 * 添加浏览历史
-		 * @param data
-		 * @returns {Promise | Promise<unknown>}
-		 */
-		get_history_add(id) {
-		    return minRequest.post('/api/histories', {article_id: id});
-		},
-		/**
-		 * 获取收藏列表
-		 * @param data
-		 * @returns {Promise | Promise<unknown>}
-		 */
-		get_collection(data) {
-		    return minRequest.get('/api/collections', data)
-		},
-		/**
-		 * 取消/收藏文章
-		 * @param data
-		 * @returns {Promise | Promise<unknown>}
-		 */
-		articles_collect(id){
-			return minRequest.put('/api/articles/' + id + '/collect')
-		},
-		/**
-		 * 获取我的关注
-		 * @param data
-		 * @returns {Promise | Promise<unknown>}
-		 */
-		get_follows(data) {
-		    return minRequest.get('/api/users/follows', data)
-		},
-		/**
-		 * 获取我的粉丝
-		 * @param data
-		 * @returns {Promise | Promise<unknown>}
-		 */
-		get_fans(data) {
-		    return minRequest.get('/api/users/fans', data)
-		},
+        /**
+         * 获取浏览历史
+         * @param data
+         * @returns {Promise | Promise<unknown>}
+         */
+        get_history(data) {
+            return minRequest.get('/api/histories', data)
+        },
+        /**
+         * 添加浏览历史
+         * @param data
+         * @returns {Promise | Promise<unknown>}
+         */
+        get_history_add(id) {
+            return minRequest.post('/api/histories', {article_id: id});
+        },
+        /**
+         * 获取收藏列表
+         * @param data
+         * @returns {Promise | Promise<unknown>}
+         */
+        get_collection(data) {
+            return minRequest.get('/api/collections', data)
+        },
+        /**
+         * 取消/收藏文章
+         * @param data
+         * @returns {Promise | Promise<unknown>}
+         */
+        articles_collect(id) {
+            return minRequest.put('/api/articles/' + id + '/collect')
+        },
+        /**
+         * 获取我的关注
+         * @param data
+         * @returns {Promise | Promise<unknown>}
+         */
+        get_follows(data) {
+            return minRequest.get('/api/users/follows', data)
+        },
+        /**
+         * 获取我的粉丝
+         * @param data
+         * @returns {Promise | Promise<unknown>}
+         */
+        get_fans(data) {
+            return minRequest.get('/api/users/fans', data)
+        },
+        /**
+         * 订阅作者列表
+         * @param data
+         * @returns {Promise | Promise<unknown>}
+         */
         authors(data) {
             return minRequest.get('/api/users/authors', data)
         },
+        /**
+         * 订阅标签列表
+         * @param data
+         * @returns {Promise | Promise<unknown>}
+         */
         tags(data) {
             return minRequest.get('/api/tags', data)
         },
+
+
+        //===============================行情相关接口================================
+        /**
+         * 行情/市值列表
+         * @param data
+         * @returns {Promise | Promise<unknown>}
+         */
+        coins(data) {
+            // newRequest.setConfig((config) => {
+            //     config.header = {
+            //         'content-type': 'application/json',
+            //         'X-Requested-With': 'XMLHttpRequest',
+            //     }
+            //     return config
+            // })
+
+            // https://dncapi.bqiapp.com/api/coin/web-coinrank?page=1&type=-1&pagesize=100&webp=1
+
+            // data.pagesize
+            // data.page
+            data.type = -1
+            data.webp = 1
+            return newRequest.get('https://dncapi.bqiapp.com/api/coin/web-coinrank', data)
+        },
+        /**
+         * 获取单个币种
+         * @param coin
+         * @param currency
+         * @returns {Promise | Promise<unknown>}
+         */
+        coin(coin, currency) {
+            // https://www.361shipin.com/shipin/others/rate/get?coin=BTC&currency=USD
+            return newRequest.get('https://dncapi.bqiapp.com/api/coin/web-coinrank', {coin: coin, currency: currency})
+        }
+
     }
 }
